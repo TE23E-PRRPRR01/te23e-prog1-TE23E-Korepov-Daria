438c9e518c952eed52411d99a70275c84fdfe1e5
Daria Korepov <daria.korepov@elev.ntig.ga.se>
2024-09-16 14:09:33 +0200

    Writeline and Readline

---
+++ HelloWorld/Kapitel-1/lucktext/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+
+Console.WriteLine("Здрастуй жопамир!");
+
+Console.WriteLine("Ange förnamn:");
+string firstname = Console.ReadLine();
+
+Console.WriteLine("Ange efternamn:");
+string lastname = Console.ReadLine();
+
+Console.WriteLine("Ange datum:");
+string date = Console.ReadLine();
+
+Console.Write("Ange månad:");
+string month = Console.ReadLine();
+
+if (firstname == "Daria" && lastname == "Korepov" && date == "7" && month == "7")
+{
+    Console.WriteLine("Добро пожаловать Мама!");
+}
+if (firstname != "Daria" && lastname != "Korepov" && date != "7" && month != "7")
+{
+    Console.WriteLine("ТЫ ПИДАР!:O");
+}
---
+++ HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+
+Console.WriteLine("Hej världen!");
+Console.WriteLine("Nu mitt första C#-program");
+Console.Write("Vad heter du?");
+string namn = Console.ReadLine();
+Console.WriteLine("Hej" + namn);
+Console.WriteLine("What's ur hobby?");
+
+Console.BackgroundColor = ConsoleColor.Black;
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("Sår du på OS i sommar?");
+Console.WriteLine("Viken sport gillar du mest?");
