f52dab81f160926fed8a9e88413a707096e0e50a
Daria Korepov <daria.korepov@elev.ntig.ga.se>
2024-12-16 14:50:00 +0100

    mikroovning4.4

---
+++ HelloWorld/Kapitel-5/Listor2/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.Clear();
+Console.WriteLine("Allt om listor");
+
+// Skapa en tom lista 
+List<string> listaNamn = [];
+
+// Infoga i en lista
+listaNamn.Add("Alex");  // verb
+listaNamn.Add("Alexandra");
+Console.WriteLine($"Antal: {listaNamn.Count}");
+
+//Skriv ut listan
+Console.WriteLine($"Lista: {listaNamn}");
+
+// Skriv ut listan mha en loop
+foreach (var namn in listaNamn)
+{
+    Console.WriteLine($"- {namn}");
+}
+
+// Skriv ut första elementet i listan (index = 0)
+Console.WriteLine($"Första elementet: {listaNamn[0]}");
+
+
+// Skriv ut två namn ur listan 
+for (var i = 0; i < 2; i++)
+{
+    Console.WriteLine($"{i}: {listaNamn[i]}");
+}
+
+// Skriv ut alla namn i listan med for-loop
+for (int i = 0; i < listaNamn.Count; i++)
+{
+    
+}
---
+++ HelloWorld/Kapitel-5/mikroovning4.4/Program.cs
@@ -0,0 +1,54 @@
+﻿Console.Clear();
+
+//Skapa en lista med action-spel
+List<string> actionGames = [];
+// Skapa en lista med äventyrsspel
+List<string> adventureGames = [];
+
+
+// Användaren fyller på listor
+//Användaren fyller på lista med action-spel
+Console.WriteLine("Nu ska du fylla i två action-spel i listan:");
+for (int i = 0; i < 2; i++)
+{
+    Console.Write("Ange ett action-spel: ");
+    actionGames.Add(Console.ReadLine());
+}
+//Användaren fyller på lista med äventyrsspel med for-loop
+Console.WriteLine("Nu ska du fylla i tre äventyrsspel:");
+for (int i = 0; i < 3; i++)
+{
+    Console.Write("Ange ett äventyrsspel: ");
+    actionGames.Add(Console.ReadLine());
+}
+
+// Användaren fyller på lista med action-spel med for-loop
+Console.WriteLine("Här är de två första action-spelen i listan:");
+Console.WriteLine("- Spel 1: " + actionGames[0]);
+Console.WriteLine("- Spel 2: " + actionGames[1]);
+
+// Skriv ut två sista äventyrsspel
+Console.WriteLine("Här är de två sista äventyrsspelen i listan:");
+Console.WriteLine("- Spel 3: " + adventureGames[adventureGames.Count - 2]);
+Console.WriteLine("- Spel 4: " + adventureGames[adventureGames.Count - 1]);
+
+// Skriv ut alla spel i varje kategori
+Console.WriteLine("Här är alla action-spel i listan:");
+foreach (string game in actionGames)
+{
+    Console.WriteLine("- " + game);
+}
+
+Console.WriteLine("Här är alla äventyrsspel i listan:");
+foreach (string game in adventureGames)
+{
+    Console.WriteLine("- " + game);
+}
+
+//Skriv ut de tre första spelen i listan mha for-loop
+Console.WriteLine("Här är de tre första action-spelen i listan: ");
+for (var i = 0; i < 3; i++)
+{
+    Console.WriteLine($"- Spel 1: {adventureGames[i]}");
+    Console.WriteLine($"- Spel 2: ");
+}
