992dc8729bbbf54604da9f7a243274cfb241d456
Daria Korepov <daria.korepov@elev.ntig.ga.se>
2025-01-08 15:04:03 +0100

    Tränat på metoder

---
+++ HelloWorld/Kapitel-6/EnMassaMetoder/Program.cs
@@ -0,0 +1,36 @@
+﻿/* *******************************************************************
+* ****************************** MAIN ********************************
+********************************************************************* */
+
+Console.Clear();
+Console.WriteLine("Hej hej Metoder!");
+
+SägHejNamn("Daria");
+SägHejNamn("Cecilia");
+SägHejNamn("");
+SägHejNamn("Liza");
+KvadreraTal('2');
+
+
+/* *******************************************************************
+* **************************** METODER *******************************
+********************************************************************* */
+
+/// <summary>
+/// Skriv ut hälsning
+/// </summary>
+/// <param name="namn">Namnet</param>
+static void SägHejNamn(string namn)
+{
+    Console.WriteLine($"Hej på dig {namn}!");
+}
+
+/// <summary>
+/// Metoden som skriver ett tal i kvadrat
+/// </summary>
+/// <param name="tal"></param>
+
+static void KvadreraTal(int tal)
+{
+    Console.WriteLine($"{tal} i kvadrat = {tal * tal}");
+}
---
+++ HelloWorld/Kapitel-6/MetoderIntro/Program.cs
@@ -0,0 +1,61 @@
+﻿/* *******************************************************************
+* ***************************** MAIN *********************************
+********************************************************************* */
+
+Console.Clear();
+Console.WriteLine("Metoder!");
+
+// Anropa/använd mentoden
+SägHej();
+SägHej();
+SägHej();
+SägHej();
+SägHej();
+SägHej();
+
+// Anropa metoden meny
+VisaMeny();
+
+// Anropa metoden RegistreraNamn()
+RegistreraNamn();
+
+
+
+
+/* *******************************************************************
+* **************************** METODER *******************************
+********************************************************************* */
+
+/// <summary>
+/// Metod som skriver ut en hälsning
+/// </summary>
+static void SägHej()
+{
+    Console.WriteLine("Hej på dig!");
+}
+
+// // Anropa SägHej() metoden igen!
+//// SägHej();
+
+/// <summary>
+/// Metod för tt visa en meny
+/// </summary>
+static void VisaMeny()
+{
+    Console.WriteLine("""
+    1. Lägg till uppgift
+    2. Ta bort uppgift
+    3. Skriv ut alla uppgifter
+    4. Avsluta
+    """);
+}
+
+/// <summary>
+/// Metod för att ange namn
+/// </summary>
+static void RegistreraNamn()
+{
+    Console.Write("Ange namn: ");
+    string namn = Console.ReadLine();
+    Console.WriteLine(namn);
+}
